b6bd6a82353ebfe7c6e27eacc37875c8334f1f85
vince.dev <Vincent.silvestri@hotmail.fr>
2026-01-02 18:13:47 +0100

    Delete les_toutes_premi_res_tapes/no_content directory

--- les_toutes_premi_res_tapes/no_content/main.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-color_names = ['Red', 'Black', 'Gray', 'Yellow', 'White', 'Green']
-
-# Write your code below
-colors = pd.Series(color_names)
-
-# Testing the result
-print(colors)

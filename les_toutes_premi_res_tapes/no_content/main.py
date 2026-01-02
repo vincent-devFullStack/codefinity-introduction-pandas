14b5e855d5734fa1773caca96ccc4df1bb4a0063
vincent-devFullStack <>
2026-01-02 17:06:42 +0000

    No content

---
+++ les_toutes_premi_res_tapes/no_content/main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+color_names = ['Red', 'Black', 'Gray', 'Yellow', 'White', 'Green']
+
+# Write your code below
+colors = pd.Series(color_names)
+
+# Testing the result
+print(colors)
